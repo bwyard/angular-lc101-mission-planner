6b7365752a5a1243d75f53afe14e4681253994b6
bwyard <45057171+bwyard@users.noreply.github.com>
2020-04-02 22:50:05 -0500

    BONUS MISSIONS

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -6,40 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
-   nearMaxMass: boolean = false;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  nearMaxMass: boolean = false;
 
-   constructor() { }
+  constructor() { }
 
-   ngOnInit() { }
+  ngOnInit() { }
 
-   // Code your addItem function here:
-   addItem(equipment: object): boolean{
-     this.cargoHold.push(equipment);
-     this.cargoMass += equipment.mass;
-     this.nearMaxMass = ((this.maximumAllowedMass-200) < this.cargoMass && this.cargoMass < (this.maximumAllowedMass) ? true : false)
-     console.log((this.maximumAllowedMass-200) < this.cargoMass && this.cargoMass < (this.maximumAllowedMass) ? true : false)
+  // Code your addItem function here:
+  addItem(equipment: object): boolean {
+    let included: number = 0;
+    for (let includedEquip of this.cargoHold) {
+      if (includedEquip === equipment) {
+        included++
+      }
+    }
 
-     // return 1800 < this.cargoMass && this.cargoMass < 2000 ? true : false;
-     return this.nearMaxMass;
-   }
-   emptyHold(){
-     this.cargoHold= [];
-     this.cargoMass = 0;
-   }
+
+    if (included < 2) {
+      this.cargoHold.push(equipment);
+      this.cargoMass += equipment.mass;
+      this.nearMaxMass = ((this.maximumAllowedMass - 200) < this.cargoMass && this.cargoMass < (this.maximumAllowedMass) ? true : false)
+    }
+    // console.log((this.maximumAllowedMass-200) < this.cargoMass && this.cargoMass < (this.maximumAllowedMass) ? true : false)
+
+    // return 1800 < this.cargoMass && this.cargoMass < 2000 ? true : false;
+    return this.nearMaxMass;
+  }
+  emptyHold() {
+    this.cargoHold = [];
+    this.cargoMass = 0;
+  }
+  removeItem(equipment, index){
+    // console.log(equipment.name)
+    //console.log(equipment)
+    this.cargoHold.splice(index,1);
+    // console.log(equipment.mass)
+    this.cargoMass -= equipment.mass;
+    this.nearMaxMass = ((this.maximumAllowedMass - 200) < this.cargoMass && this.cargoMass < (this.maximumAllowedMass) ? true : false)
+    // console.log(equipment.mass)
+  }
 
 }
